6ca6f0fe18ab1a81bfac92ce93b4b66eb63300fd
Jorge Brunetto <jorge.brunetto@bluecore.it>
2020-03-27 00:06:36 -0300

    Enviando parametros pelo get

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -1,10 +1,36 @@
+// Importar o express para dentro do projeto
 const express = require("express");
 
+// Inicializar o express
 const app = express();
-app.get("/", (req, res) => {
+
+
+//Estou querendo acessar o recurso de usuários  '/users'
+/**
+ * Metodos HTTP
+ * 
+ * GET: Buscar informações do meu back
+ * POST: Criar uma informação no back
+ * PUT: Alterar uma informação no back
+ * DELETE: Apagar uma informação do meu back
+*/
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Parametros nomeados enviados na rota após "?"
+ * Route Params: Identificar um único recurso depois da "/"
+ * Request Body: 
+ */
+
+app.get("/users", (req, res) => {
+    const params = req.query;
+    console.log(params)
     return res.json({
         evento: "Semana OmniStack 11.0",
         aluno: "Jorge Brunetto"
     });
 });
+
+//Cria nosso server na porta sugerida 
 app.listen(3333);
