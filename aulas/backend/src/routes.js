84ddb6c0557618e1ec9ff5f2b32e26e7b2c70454
Jorge Brunetto <jorge.brunetto@bluecore.it>
2020-03-27 01:10:36 -0300

    Cadastro e Listagem de ongs

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,8 +1,8 @@
 // Importar o express para dentro do projeto
 const express = require("express");
-const crypto = require('crypto');
+const crypto = require("crypto");
 const routes = express.Router();
-const connection = require('./database/connection')
+const connection = require("./database/connection");
 //Estou querendo acessar o recurso de usuários  '/users'
 /**
  * Metodos HTTP
@@ -25,14 +25,26 @@ const connection = require('./database/connection')
  * Usar Query Builder: table('users').select('*').where('name=jorge')
  * vamos usar o KNEX.JS
  */
+routes.get("/ongs", async (req, res) => {
+    const ongs = await connection("ongs").select("*");
 
-routes.post("/ongs", (req, res) => {
+    return res.json(ongs);
+});
+
+routes.post("/ongs", async (req, res) => {
     const { name, email, whatsapp, city, uf } = req.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = crypto.randomBytes(4).toString("HEX");
 
+    await connection("ongs").insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    });
 
-
-    return res.json();
+    return res.json({ id });
 });
 module.exports = routes;
