51e3ca8ef048087bdcc1268ecff742488eff6076
Jorge Brunetto <jorge.brunetto@bluecore.it>
2020-03-26 22:50:19 -0300

    Primeiro commit com "Node, Evento e nome do Aluno"

---
+++ aulas/backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+    return res.json({
+        evento: "Semana OmniStack 11.0",
+        aluno: "Jorge Brunetto"
+    });
+});
+app.listen(3333);
