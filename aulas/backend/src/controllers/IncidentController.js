48712abacd383dd3eba7dbf820be1e9181eed233
Jorge Brunetto <jorgebrunetto@hotmail.com>
2020-03-28 12:26:22 -0300

    Adicionado o Cors para segurança de requisição atraves da URL determinada

--- aulas/backend/src/controllers/IncidentController.js
+++ aulas/backend/src/controllers/IncidentController.js
@@ -6,11 +6,18 @@ module.exports = {
         const { page = 1 } = req.query;
         const [count] = await connection("incidents").count();
         const incidents = await connection("incidents")
-            .select("*")
+            .join("ongs", "ongs.id", "=", "incidents.ong_id")
             .limit(5)
-            .offset((page - 1) * 5);
+            .offset((page - 1) * 5)
+            .select([
+                "incidents.*",
+                "ongs.name",
+                "ongs.email",
+                "ongs.city",
+                "ongs.uf"
+            ]);
 
-        res.header('X-Total-Count', count['count(*)'])
+        res.header("X-Total-Count", count["count(*)"]);
         return res.json(incidents);
     },
 
--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,11 +1,18 @@
 // Importar o express para dentro do projeto
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 // Inicializar o express
 const app = express();
+
+// Indico atraves do origen: qual a URL que terá acesso a API
+app.use(cors());
+
+// Indica que os retornos do express seram em JSON
 app.use(express.json());
 
+// Inicializa as rotas
 app.use(routes);
 
 //Cria nosso server na porta sugerida
