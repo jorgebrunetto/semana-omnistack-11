87a6f311fa5705b887fb2f4918446c7cfa555ca3
Jorge Brunetto <jorgebrunetto@hotmail.com>
2020-03-29 12:23:06 -0300

    - Passando valores no value para o useState

--- aulas/frontend/src/pages/Register/index.js
+++ aulas/frontend/src/pages/Register/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
 
 import "./styles.css";
 import logoImg from "../../assets/logo.svg";
 
 export default function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [whatsapp, setWhatsapp] = useState("");
+    const [city, setCity] = useState("");
+    const [uf, setUf] = useState("");
+
+    function handleRegister(e) {
+        e.preventDefault();
+        console.log({ name, email });
+    }
     return (
         <div className="register-container">
             <div className="content">
@@ -21,16 +32,42 @@ export default function Register() {
                         Voltar para o logon
                     </Link>
                 </section>
-                <form>
-                    <input type="text" placeholder="Nome da ONG" />
-                    <input type="email" placeholder="E-mail" />
-                    <input type="text" placeholder="WhatsApp" />
+                <form onSubmit={handleRegister}>
+                    <input
+                        type="text"
+                        placeholder="Nome da ONG"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="WhatsApp"
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
 
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{ width: 80 }} />
+                        <input
+                            placeholder="Cidade"
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <input
+                            placeholder="UF"
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                            style={{ width: 80 }}
+                        />
                     </div>
-                    <button type="submit" class="button">Cadastrar</button>
+                    <button type="submit" class="button">
+                        Cadastrar
+                    </button>
                 </form>
             </div>
         </div>
