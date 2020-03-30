a645fbbd7cf00c90625d31e44fc3c7acf4befe31
Jorge Brunetto <jorgebrunetto@hotmail.com>
2020-03-30 02:41:59 -0300

    Adicionado o Flatlist (Scroll) em um renderItem

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, Image, Text, TouchableOpacity, FlatList } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import logoImg from "../../assets/logo.png";
 
-import './styles';
+import styles from "./styles";
 
 export default function Incidents() {
-  return (
-    <View />
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>1 casos</Text>.
+                </Text>
+            </View>
+            <Text style={styles.title}>Bem-vindo!</Text>
+            <Text style={styles.description}>
+                Escolha um dos casos abaixo e salve o dia.
+            </Text>
+
+            <FlatList
+                style={styles.incidentsList}
+                data={[1, 2, 3]}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>
+                            Clçadfj çalsdkj{" "}
+                        </Text>
+
+                        <Text style={styles.incidentProperty}>Valor:</Text>
+                        <Text style={styles.incidentValue}>120</Text>
+
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={() => {}}
+                        >
+                            <Text style={styles.detailsButtonText}>
+                                Ver mais detalhes
+                            </Text>
+                            <Feather
+                                name="arrow-right"
+                                size={16}
+                                color="#e02041"
+                            />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </View>
+    );
 }
