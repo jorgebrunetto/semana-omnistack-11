fb5648fb1ae8e38c56ff364af222791610e6c1d8
Jorge Brunetto <jorgebrunetto@hotmail.com>
2020-03-31 01:07:35 -0300

    - Envio via Whatsapp

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -1,16 +1,38 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+
+// Lib para que possa enviar e abrir o email
+import * as MailComposer from "expo-mail-composer";
+
+// DIP Link você usa uma URL
+
 import logoImg from "../../assets/logo.png";
 
 import styles from "./styles";
 
 export default function Detail() {
+    const navigation = useNavigation();
+    const message = "Ola APAD, estou entrando em contato pois...";
+    function navigateBack() {
+        navigation.goBack();
+    }
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: "Tesskljflasdjç",
+            recipients: ["jorge@otmasd.com"],
+            body: message
+        });
+    }
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=5515981376495&text=${message}`);
+    }
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather name="arrow-left" size={28} color="#e02041" />
                 </TouchableOpacity>
             </View>
@@ -26,16 +48,16 @@ export default function Detail() {
                 <Text style={styles.incidentProperty}>Valor:</Text>
                 <Text style={styles.incidentValue}>120</Text>
             </View>
-            <View>
+            <View style={styles.contactBox}>
                 <Text style={styles.heroTitle}>ONG:</Text>
                 <Text style={styles.heroTitle}>ONG:</Text>
 
                 <Text style={styles.heroDescription}>120</Text>
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
--- aulas/mobile/src/pages/Detail/styles.js
+++ aulas/mobile/src/pages/Detail/styles.js
@@ -15,6 +15,7 @@ export default StyleSheet.create({
         padding: 24,
         borderRadius: 8,
         backgroundColor: "#fff",
+        marginTop: 48,
         marginBottom: 16
     },
     incidentProperty: {
@@ -27,5 +28,40 @@ export default StyleSheet.create({
         marginTop: 8,
         fontSize: 15,
         color: "#737380"
+    },
+    contactBox: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: "#fff",
+        marginBottom: 16
+    },
+    heroTitle: {
+        fontWeight: "bold",
+        fontSize: 20,
+        color: "#13131A",
+        lineHeight: 30
+    },
+    heroDescription: {
+        fontSize: 15,
+        color: "#737380",
+        marginTop: 16
+    },
+    actions: {
+        marginTop: 16,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    action: {
+        backgroundColor: "#e02041",
+        borderRadius: 8,
+        height: 50,
+        width: "40%",
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    actionText:{
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold'
     }
 });
