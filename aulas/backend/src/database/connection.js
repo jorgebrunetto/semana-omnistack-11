377a7f96ad11082d0d088b731aa94e288ca47b6c
Jorge Brunetto <jorgebrunetto@hotmail.com>
2020-03-31 22:04:32 -0300

    - Usei o npm install jest - Para inicializar os testes npx jest --init - Para rodar os testes npm test que foi configurada na package

--- aulas/backend/src/database/connection.js
+++ aulas/backend/src/database/connection.js
@@ -1,7 +1,13 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const configuration = require('../../knexfile');
+const configuration = require("../../knexfile");
 
-const connection = knex(configuration.development);
+// variavel global gerada pelo component cross no package.json
+const config =
+    process.env.NODE_ENV === "test"
+        ? configuration.test
+        : configuration.development;
+
+const connection = knex(config);
 
 module.exports = connection;
