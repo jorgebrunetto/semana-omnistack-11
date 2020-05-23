d89b15b187f6d9254a18fd3fccc47a462da1c30e
Jorge Brunetto <jorgebrunetto@hotmail.com>
2020-05-22 22:44:16 -0300

    Correção dos caminhos para rodar

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 
+
 import api from "../../services/api";
 
 import "./styles.css";
